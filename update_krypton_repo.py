a15916a702782f95a55165e5f73c744eb1123c3f
bumpaneer <bumpaneer@gmail.com>
2017-02-24 08:25:18 -0500

    update krypton repo script

--- update_krypton_repo.py
+++ update_krypton_repo.py
@@ -1,21 +1,47 @@
 import os
 import zipfile
 from git import Repo
+import hashlib
 import shutil
 import stat
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
+from pathlib import Path
 import sys
 import subprocess
 
 def zipdir(path, zip):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            zip.write(os.path.join(root, file))
+	for root, dirs, files in os.walk(path):
+		for file in files:
+			zip.write(os.path.join(root, file))
 
 def del_rw(action, name, exc):
-    os.chmod(name, stat.S_IWRITE)
-    os.remove(name)
-		
+	os.chmod(name, stat.S_IWRITE)
+	os.remove(name)
+
+def generate_md5_file( file ):
+	# create a new md5 hash
+	hash_md5 = hashlib.md5()
+	with open(file, "rb") as f:
+		for chunk in iter(lambda: f.read(4096), b""):
+			hash_md5.update(chunk)
+	m = hash_md5.hexdigest()
+	# append file name
+	m = m + "  " + os.path.basename(file)
+	# save file
+	try:
+		save_file( m.encode( "UTF-8" ), file=file + ".md5" )
+	except Exception as e:
+		# oops
+		print("An error occurred creating " + file + ".md5 file!\n%s" % e)
+
+def save_file( data, file ):
+	try:
+		# write data to the file (use b for Python 3)
+		open( file, "wb" ).write( data )
+	except Exception as e:
+		# oops
+		print("An error occurred saving %s file!\n%s" % ( file, e ))
+
 if __name__ == '__main__':
 		if( not os.path.isdir('krypton\\working')):
 			os.mkdir('krypton\\working')
@@ -23,34 +49,29 @@ if __name__ == '__main__':
 		shutil.rmtree('krypton\\working\\repo\\.git', onerror=del_rw)
 		os.remove('krypton\\working\\repo\\.gitattributes')
 		os.remove('krypton\\working\\repo\\.gitignore')
-		e = minidom.parse('krypton\\working\\repo\\addon.xml')
-		addoninfo = e.getElementsByTagName('addon')
-		addonid = addoninfo[0].attributes['id'].value
-		addonversion = addoninfo[0].attributes['version'].value
+		e = ET.parse('krypton\\working\\repo\\addon.xml').getroot()
+		addonid = e.get('id')
+		addonversion = e.get('version')
 		filestring = 'krypton\\working\\' + addonid + '-' + addonversion + '.zip'
 		newdir = 'krypton\\working\\' + addonid
 		if( not os.path.isdir('krypton\\' + addonid)):
 			os.mkdir('krypton\\' + addonid)
+		else:
+			for root, dirs, files in os.walk('krypton\\' + addonid, topdown=False):
+				for name in files:
+					if( not name.endswith('.zip') and not name.endswith('.md5')):
+						os.remove(os.path.join(root, name))
+				for name in dirs:
+					os.rmdir(os.path.join(root, name))
+		for asset in e.findall(".//*[@point='xbmc.addon.metadata']/assets/*"):
+			assetdir = os.path.dirname(asset.text).replace('/', '\\')
+			assetfile = os.path.basename(asset.text)
+			if(os.path.exists('krypton\\working\\repo\\' + assetdir + '\\' + assetfile)):
+				path = Path('krypton\\' + addonid + '\\' + assetdir)
+				path.mkdir(parents=True, exist_ok=True)
+				shutil.copy2('krypton\\working\\repo\\' + assetdir + '\\' + assetfile, 'krypton\\' + addonid + '\\' + assetdir)
 		if(os.path.exists('krypton\\working\\repo\\addon.xml')):
 			shutil.copy2('krypton\\working\\repo\\addon.xml', 'krypton\\' + addonid)
-		if(os.path.exists('krypton\\working\\repo\\resources\\fanart.jpg')):
-			shutil.copy2('krypton\\working\\repo\\resources\\fanart.jpg', 'krypton\\' + addonid)
-		if(os.path.exists('krypton\\working\\repo\\resources\\icon.png')):
-			shutil.copy2('krypton\\working\\repo\\resources\\icon.png', 'krypton\\' + addonid)
-		if(os.path.exists('krypton\\working\\repo\\changelog.txt')):
-			shutil.copy2('krypton\\working\\repo\\changelog.txt', 'krypton\\' + addonid + '\\changelog-' + addonversion + '.txt')
-		if(os.path.isdir('krypton\\working\\repo\\resources')):
-			if(os.path.exists('krypton\\' + addonid + '\\screenshots.zip')):
-				os.remove('krypton\\' + addonid + '\\screenshots.zip')
-			os.mkdir(newdir)
-			shutil.move('krypton\\working\\repo\\resources', newdir + '\\resources')
-			os.chdir('krypton\\working\\')
-			zipscreen = zipfile.ZipFile('screenshots.zip', 'w', zipfile.ZIP_DEFLATED)
-			zipdir(addonid, zipscreen)
-			zipscreen.close()
-			os.chdir('..\\..')
-			shutil.rmtree(newdir, onerror=del_rw)
-			shutil.move('krypton\\working\\screenshots.zip', 'krypton\\' + addonid)
 		theproc = subprocess.Popen(["TexturePacker.exe", "-dupecheck -input krypton\\working\\repo\\media\\ -output krypton\\working\\Textures.xbt"])
 		theproc.communicate()
 		shutil.rmtree('krypton\\working\\repo\\media', onerror=del_rw)
@@ -69,3 +90,7 @@ if __name__ == '__main__':
 		nextproc = subprocess.Popen([sys.executable, "addons_xml_generator.py"])
 		nextproc.communicate()
 		os.chdir('..')
+		if(os.path.exists('krypton\\' + addonid + '\\' + addonid + '-' + addonversion + '.zip')):
+			generate_md5_file('krypton\\' + addonid + '\\' + addonid + '-' + addonversion + '.zip')
+		if(os.path.exists('krypton\\' + addonid + '\\addon.xml')):
+			os.remove('krypton\\' + addonid + '\\addon.xml')
